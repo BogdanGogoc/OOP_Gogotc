d9a87eaf241a6840bf7bee32e630a23bc5561051
BogdanGogoc <99424304+BogdanGogoc@users.noreply.github.com>
2023-05-17 13:12:08 +0300

    Add files via upload

---
+++ Lab_4/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        string fileName = "text.txt";
+
+        while (true)
+        {
+            Console.Write("Введіть текст: ");
+            string text = Console.ReadLine();
+            File.WriteAllText(fileName, text);
+            Console.WriteLine();
+
+            char[] fileContent = File.ReadAllText(fileName).ToLower().ToCharArray();
+            int vowelsCount = CountVowels(fileContent);
+            int consonantsCount = CountConsonants(fileContent);
+            Console.WriteLine("Кількість голосних літер: " + vowelsCount);
+            Console.WriteLine("Кількість приголосних літер: " + consonantsCount);
+
+            Console.WriteLine();
+            Console.Write("0 для виходу: ");
+            text = Console.ReadLine();
+            if (text == "0") Environment.Exit(0);
+            Console.WriteLine();
+        }
+    }
+
+    static int CountVowels(char[] charArray)
+    {
+        List<char> vowels = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
+        int count = 0;
+        foreach (char c in charArray)
+        {
+            if (vowels.Contains(c))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static int CountConsonants(char[] charArray)
+    {
+        List<char> consonants = new List<char>() { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+        int count = 0;
+        foreach (char c in charArray)
+        {
+            if (consonants.Contains(c))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
